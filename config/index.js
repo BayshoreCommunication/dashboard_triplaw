c35feff2c312e42e7651e6a7b761c23c036370b4
arsahak <arsahak99@gmail.com>
2024-03-04 16:11:47 +0600

    first commit

---
+++ config/index.js
@@ -0,0 +1,4 @@
+export const API_URL = process.env.NEXT_PUBLIC_API_URL;
+export const CLOUDINARY_CLOUD_NAME = "dv5vujozp";
+export const CLOUDINARY_UPLOAD_PRESET = "apexadvisor";
+export const CLOUDINARY_FOLDER = "apexadvisor";

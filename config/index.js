3590eb858adf7712a95919e0a478797e20159a92
arsahak <arsahak99@gmail.com>
2025-02-06 14:02:16 +0600

    Add initial Next.js configuration files and update environment variables

--- config/index.js
+++ config/index.js
@@ -1,4 +1,4 @@
 export const API_URL = process.env.NEXT_PUBLIC_API_URL;
-export const CLOUDINARY_CLOUD_NAME = "dmrwj8j4j";
+export const CLOUDINARY_CLOUD_NAME = "duuvpeoey";
 export const CLOUDINARY_UPLOAD_PRESET = "dashboard-triplaw";
 export const CLOUDINARY_FOLDER = "dashboard-triplaw";

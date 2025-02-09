9b525e44fe359d2d1fda005cd8bcb77c4730ec9f
BayshoreCommunication <134770953+BayshoreCommunication@users.noreply.github.com>
2025-02-09 09:48:37 +0600

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,4 +1,16 @@
+
 export const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
+// Old Image Upload Configuration Cloudinary
+
 export const CLOUDINARY_CLOUD_NAME = "duuvpeoey";
 export const CLOUDINARY_UPLOAD_PRESET = "dashboard-triplaw";
 export const CLOUDINARY_FOLDER = "dashboard-triplaw";
+
+
+// New Image Upload Configuration Digital Ocean
+
+export const DIGITALOCEAN_ENDPOINT = "https://nyc3.digitaloceanspaces.com";
+export const DIGITALOCEAN_API_KEY = "DO00HFNWVPYJBRTBVZZJ";
+export const DIGITALOCEAN_SECRET_ACCESS_KEY= "VTaTd4LASdw+OjDylmiBOPa4GOD956R/25597C7ULX8";
+export const DIGITALOCEAN_FOLDER = "trip-law";

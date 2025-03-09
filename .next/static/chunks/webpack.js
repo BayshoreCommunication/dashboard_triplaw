6bf43d5927393e46b73cf5ac381f251e05009d6c
arsahak <arsahak99@gmail.com>
2025-03-09 14:29:35 +0600

    Update webpack cache and remove unused image files for optimization

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -161,7 +161,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "877dd14fe9d432a4"; }
+/******/ 		__webpack_require__.h = function() { return "d145a265ceba846a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
